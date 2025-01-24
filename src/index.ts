0910b16875d1af965f00a7b0f840f7d256dc8a55
ayan4m1 <andrew@bulletlogic.com>
2025-01-24 14:16:35 -0500

    don't pre-emptively destroy client

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { Client } from 'discord.js';
 import { connectToCache } from './modules/cache.js';
 import {
   connectBot,
@@ -9,22 +8,16 @@ import { getLogger } from './modules/logging.js';
 
 const log = getLogger('index');
 
-let client: Client = null;
-
 try {
   await connectToCache();
   await syncCommands();
   await registerCommandsAndHandlers();
 
-  client = await connectBot();
+  const client = await connectBot();
   const guilds = [...client.guilds.cache.values()];
 
   log.info(`Bot is connected to ${guilds.length} servers!`);
 } catch (error) {
   log.error(error.message);
   log.error(error.stack);
-} finally {
-  if (client) {
-    await client.destroy();
-  }
 }
